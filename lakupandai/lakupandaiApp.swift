4da5a3170e229ec557a750872186d00bae4114f9
mochammadqaysa <qaysa78@gmail.com>
2023-10-04 08:32:28 +0700

    Initial Commit

---
+++ lakupandai/lakupandaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  lakupandaiApp.swift
+//  lakupandai
+//
+//  Created by Digital Amore Kriyanesia on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct lakupandaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
