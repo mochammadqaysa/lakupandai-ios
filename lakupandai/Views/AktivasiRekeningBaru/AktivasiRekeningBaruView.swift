8892c924b218ff746cc99ab8252ccdb3585ec9da
mochammadqaysa <qaysa78@gmail.com>
2023-10-14 02:42:02 +0700

    [UPDATE] finished aktivasi form

--- lakupandai/Views/AktivasiRekeningBaru/AktivasiRekeningBaruView.swift
+++ lakupandai/Views/AktivasiRekeningBaru/AktivasiRekeningBaruView.swift
@@ -41,24 +41,50 @@ struct AktivasiRekeningBaruView: View {
     var formActivation : some View {
         return VStack{
             Group {
-                VStack {
-                    TextField("Nomor Induk Kependudukan", text: $aktivasiRekeningBaruVM.nik)
+                VStack{
+                    ZStack(alignment: .leading) {
+                        Text("Nomor Induk Kependudukan")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.nik.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.nik.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.nik)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.nik.isEmpty ? 0 : 22)
+                    
                     Divider()
                         .background(.black)
                         .tint(.black)
                         .foregroundColor(.black)
                 }
                 .padding(.vertical)
-                VStack {
-                    TextField("Nama", text: $aktivasiRekeningBaruVM.namaDebitur)
+                
+                VStack{
+                    ZStack(alignment: .leading) {
+                        Text("Nama")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.namaDebitur.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.namaDebitur.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.namaDebitur)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.namaDebitur.isEmpty ? 0 : 22)
+                    
                     Divider()
                         .background(.black)
                         .tint(.black)
                         .foregroundColor(.black)
                 }
                 .padding(.vertical)
+                
                 VStack {
-                    TextField("Jenis Kelamin", text: $aktivasiRekeningBaruVM.jenisKelamin)
+                    ZStack(alignment: .leading) {
+                        Text("Jenis Kelamin")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.jenisKelamin.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.jenisKelamin.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.jenisKelamin)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.jenisKelamin.isEmpty ? 0 : 22)
+                    
                     Divider()
                         .background(.black)
                         .tint(.black)
@@ -67,14 +93,28 @@ struct AktivasiRekeningBaruView: View {
                 .padding(.vertical)
                 HStack(){
                     VStack {
-                        TextField("Tempat Lahir", text: $aktivasiRekeningBaruVM.tempatLahir)
+                        ZStack(alignment: .leading) {
+                            Text("Tempat Lahir")
+                                .foregroundColor(.gray.opacity(0.5))
+                                .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.tempatLahir.isEmpty)
+                                .offset(y: aktivasiRekeningBaruVM.tempatLahir.isEmpty ? 0 : -20)
+                            TextField("", text: $aktivasiRekeningBaruVM.tempatLahir)
+                        }
+                        .padding(.top, aktivasiRekeningBaruVM.tempatLahir.isEmpty ? 0 : 22)
                         Divider()
                             .background(.black)
                             .tint(.black)
                             .foregroundColor(.black)
                     }
                     VStack {
-                        TextField("Tanggal Lahir", text: $aktivasiRekeningBaruVM.tanggalLahir)
+                        ZStack(alignment: .leading) {
+                            Text("Tanggal Lahir")
+                                .foregroundColor(.gray.opacity(0.5))
+                                .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.tanggalLahir.isEmpty)
+                                .offset(y: aktivasiRekeningBaruVM.tanggalLahir.isEmpty ? 0 : -20)
+                            TextField("", text: $aktivasiRekeningBaruVM.tanggalLahir)
+                        }
+                        .padding(.top, aktivasiRekeningBaruVM.tanggalLahir.isEmpty ? 0 : 22)
                         Divider()
                             .background(.black)
                             .tint(.black)
@@ -83,7 +123,14 @@ struct AktivasiRekeningBaruView: View {
                 }
                 .padding(.vertical)
                 VStack {
-                    TextField("Kode Pekerjaan", text: $aktivasiRekeningBaruVM.kodePekerjaan)
+                    ZStack(alignment: .leading) {
+                        Text("Kode Pekerjaan")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.kodePekerjaan.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.kodePekerjaan.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.kodePekerjaan)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.kodePekerjaan.isEmpty ? 0 : 22)
                     Divider()
                         .background(.black)
                         .tint(.black)
@@ -91,7 +138,14 @@ struct AktivasiRekeningBaruView: View {
                 }
                 .padding(.vertical)
                 VStack {
-                    TextField("Kota / Kabupaten", text: $aktivasiRekeningBaruVM.kotaKab)
+                    ZStack(alignment: .leading) {
+                        Text("Kota / Kabupaten")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.kotaKab.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.kotaKab.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.kotaKab)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.kotaKab.isEmpty ? 0 : 22)
                     Divider()
                         .background(.black)
                         .tint(.black)
@@ -100,14 +154,29 @@ struct AktivasiRekeningBaruView: View {
                 .padding(.vertical)
                 HStack(){
                     VStack {
-                        TextField("Kecamatan", text: $aktivasiRekeningBaruVM.kecamatan)
+                        ZStack(alignment: .leading) {
+                            Text("Kecamatan")
+                                .foregroundColor(.gray.opacity(0.5))
+                                .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.kecamatan.isEmpty)
+                                .offset(y: aktivasiRekeningBaruVM.kecamatan.isEmpty ? 0 : -20)
+                            TextField("", text: $aktivasiRekeningBaruVM.kecamatan)
+                        }
+                        .padding(.top, aktivasiRekeningBaruVM.kecamatan.isEmpty ? 0 : 22)
+                        
                         Divider()
                             .background(.black)
                             .tint(.black)
                             .foregroundColor(.black)
                     }
                     VStack {
-                        TextField("Kelurahan / Desa", text: $aktivasiRekeningBaruVM.kelurahanDes)
+                        ZStack(alignment: .leading) {
+                            Text("Kelurahan / Desa")
+                                .foregroundColor(.gray.opacity(0.5))
+                                .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.kelurahanDes.isEmpty)
+                                .offset(y: aktivasiRekeningBaruVM.kelurahanDes.isEmpty ? 0 : -20)
+                            TextField("", text: $aktivasiRekeningBaruVM.kelurahanDes)
+                        }
+                        .padding(.top, aktivasiRekeningBaruVM.kelurahanDes.isEmpty ? 0 : 22)
                         Divider()
                             .background(.black)
                             .tint(.black)
@@ -116,7 +185,14 @@ struct AktivasiRekeningBaruView: View {
                 }
                 .padding(.vertical)
                 VStack {
-                    TextField("Status Perkawinan", text: $aktivasiRekeningBaruVM.statusPerkawinan)
+                    ZStack(alignment: .leading) {
+                        Text("Status Perkawinan")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.statusPerkawinan.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.statusPerkawinan.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.statusPerkawinan)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.statusPerkawinan.isEmpty ? 0 : 22)
                     Divider()
                         .background(.black)
                         .tint(.black)
@@ -124,7 +200,14 @@ struct AktivasiRekeningBaruView: View {
                 }
                 .padding(.vertical)
                 VStack {
-                    TextField("Nama Ibu Kandung", text: $aktivasiRekeningBaruVM.namaIbu)
+                    ZStack(alignment: .leading) {
+                        Text("Nama Ibu Kandung")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.namaIbu.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.namaIbu.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.namaIbu)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.namaIbu.isEmpty ? 0 : 22)
                     Divider()
                         .background(.black)
                         .tint(.black)
@@ -134,7 +217,15 @@ struct AktivasiRekeningBaruView: View {
             }
             Group {
                 VStack {
-                    TextField("Kode Pendidikan", text: $aktivasiRekeningBaruVM.kodePendidikan)
+                    ZStack(alignment: .leading) {
+                        Text("Kode Pendidikan")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.kodePendidikan.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.kodePendidikan.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.kodePendidikan)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.kodePendidikan.isEmpty ? 0 : 22)
+                    
                     Divider()
                         .background(.black)
                         .tint(.black)
@@ -143,7 +234,14 @@ struct AktivasiRekeningBaruView: View {
                 .padding(.vertical)
                 
                 VStack {
-                    TextField("No Telp", text: $aktivasiRekeningBaruVM.nomorTelp)
+                    ZStack(alignment: .leading) {
+                        Text("No Telp")
+                            .foregroundColor(.gray.opacity(0.5))
+                            .animation(.linear(duration: 0.3), value: aktivasiRekeningBaruVM.nomorTelp.isEmpty)
+                            .offset(y: aktivasiRekeningBaruVM.nomorTelp.isEmpty ? 0 : -20)
+                        TextField("", text: $aktivasiRekeningBaruVM.nomorTelp)
+                    }
+                    .padding(.top, aktivasiRekeningBaruVM.nomorTelp.isEmpty ? 0 : 22)
                     Divider()
                         .background(.black)
                         .tint(.black)
@@ -155,6 +253,7 @@ struct AktivasiRekeningBaruView: View {
             
         }
     }
+    
 }
 
 struct AktivasiRekeningBaruView_Previews: PreviewProvider {
